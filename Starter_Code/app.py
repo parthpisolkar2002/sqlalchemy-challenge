11a1d0b32fd667c81a8cf5b5803296eae9680a92
Parth Pisolkar <parthpisolkar@Parths-MacBook-Pro.local>
2024-05-13 19:18:56 -0400

    a few changes in app.py

--- Starter_Code/app.py
+++ Starter_Code/app.py
@@ -115,31 +115,3 @@ def start_end_date(start, end):
 if __name__ == "__main__":
     app.run(debug=True)
 
-
-
-#################################################
-# Database Setup
-#################################################
-
-
-# reflect an existing database into a new model
-
-# reflect the tables
-
-
-# Save references to each table
-
-
-# Create our session (link) from Python to the DB
-
-
-#################################################
-# Flask Setup
-#################################################
-
-
-
-
-#################################################
-# Flask Routes
-#################################################
